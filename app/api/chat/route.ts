b75995fbcf286f580d3f0c96bfe495027353a4f8
Subroh Nishikori <subroh.0508@gmail.com>
2025-10-29 21:26:08 +0900

    Initial commit

---
+++ app/api/chat/route.ts
@@ -0,0 +1,22 @@
+import { mastra } from "@/mastra";
+import { toAISdkFormat } from "@mastra/ai-sdk";
+import { createUIMessageStreamResponse, UIMessage } from "ai";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const agent = mastra.getAgent("weatherAgent");
+  const result = await agent.stream(messages);
+
+  // Return the result as a data stream response
+  // Workaround: https://discord.com/channels/1309558646228779139/1413241662091694100/1425928259513749554
+  return createUIMessageStreamResponse({
+    stream: toAISdkFormat(
+      result,
+      { from: "agent" },
+    ),
+  });
+}
---
+++ mastra/index.ts
@@ -0,0 +1,23 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+  telemetry: {
+    // Telemetry is deprecated and will be removed in the Nov 4th release
+    enabled: false, 
+  },
+  observability: {
+    // Enables DefaultExporter and CloudExporter for AI tracing
+    default: { enabled: true }, 
+  },
+});
